16fb77699957bb8658862b3f888c7ea97b1919c5
ahd.bk <ahd.benkheder@gmail.com>
2023-01-01 14:49:51 +0100

    update post url

--- blog/server/server.js
+++ blog/server/server.js
@@ -13,7 +13,7 @@ async function getPosts() {
             data.date = _convertDate(data.date)
             return {
                 metaData: data,
-                path: `/${item.replace('.md', '.html')}`,
+                path: `/${item.substring(5).replace('.md', '.html')}`,
                 readingTime: readingTime(content).text
             }
         })
